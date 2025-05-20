89346c9f5a07222ee1af3c1c514e0b52c739ef89
onsissond <onsissond@gmail.com>
2025-05-20 08:56:57 +0300

    Add GitHubPrivateRepositoryReleaseDownloadStrategy

--- Formula/eventpanel.rb
+++ Formula/eventpanel.rb
@@ -1,7 +1,10 @@
+require "formula"
+require_relative "lib/private_strategy"
+
 class Eventpanel < Formula
   desc "EventPanel CLI tool"
   homepage "https://github.com/eventpanel/eventpanel-cli"
-  url "https://github.com/eventpanel/eventpanel-cli/archive/refs/tags/0.0.1.tar.gz"
+  url "https://github.com/eventpanel/eventpanel-cli/archive/refs/tags/0.0.1.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   sha256 "d046eeda781a8edec8a03518b65893056b8a8bb02f1f2602a7df64333f14a1e6"
   license "MIT"
 

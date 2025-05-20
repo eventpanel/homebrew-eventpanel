6005182a9a179e6951bda3c8d2b505b13ce26530
Evgeny <sukhanovevgeniy96@gmail.com>
2025-05-20 08:31:28 +0300

    Initial commit: Add EventPanel formula

---
+++ Formula/eventpanel.rb
@@ -0,0 +1,19 @@
+class Eventpanel < Formula
+  desc "EventPanel CLI tool"
+  homepage "https://github.com/event-panel/eventpanel-cli"
+  url "https://github.com/event-panel/eventpanel-cli/archive/refs/tags/0.0.1.tar.gz"
+  sha256 "d046eeda781a8edec8a03518b65893056b8a8bb02f1f2602a7df64333f14a1e6"
+  license "MIT"
+
+  depends_on xcode: ["14.0", :build]
+  depends_on macos: :monterey
+
+  def install
+    system "swift", "build", "--configuration", "release"
+    bin.install ".build/release/eventpanel"
+  end
+
+  test do
+    system "#{bin}/eventpanel", "--version"
+  end
+end 

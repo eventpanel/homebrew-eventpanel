b9fc23de643c7c986bc2208499e6ffedf98901e9
onsissond <onsissond@gmail.com>
2025-05-20 08:39:07 +0300

    Update the url

--- Formula/eventpanel.rb
+++ Formula/eventpanel.rb
@@ -1,7 +1,7 @@
 class Eventpanel < Formula
   desc "EventPanel CLI tool"
-  homepage "https://github.com/event-panel/eventpanel-cli"
-  url "https://github.com/event-panel/eventpanel-cli/archive/refs/tags/0.0.1.tar.gz"
+  homepage "https://github.com/eventpanel/eventpanel-cli"
+  url "https://github.com/eventpanel/eventpanel-cli/archive/refs/tags/0.0.1.tar.gz"
   sha256 "d046eeda781a8edec8a03518b65893056b8a8bb02f1f2602a7df64333f14a1e6"
   license "MIT"
 

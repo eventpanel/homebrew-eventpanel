4cea14ecbd00e971bc781531dedd0e9ea1fbadce
onsissond <onsissond@gmail.com>
2025-05-21 08:33:33 +0300

    Add header

--- Formula/eventpanel.rb
+++ Formula/eventpanel.rb
@@ -1,18 +1,19 @@
-require "formula"
-require_relative "lib/private_strategy"
-
 class Eventpanel < Formula
   desc "EventPanel CLI tool"
   homepage "https://github.com/eventpanel/eventpanel-cli"
-  url "https://github.com/eventpanel/eventpanel-cli/archive/refs/tags/0.0.1.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "d046eeda781a8edec8a03518b65893056b8a8bb02f1f2602a7df64333f14a1e6"
+  url "https://github.com/eventpanel/eventpanel-cli/archive/refs/tags/0.0.1.tar.gz",
+      headers: [
+        "Accept: application/octet-stream",
+        "Authorization: bearer #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}"
+      ]
+  sha256 "1191a5d03b9dee7a7c1ee867ded1a7b198b027c7ffa7677238c44947a27ca0d0"
   license "MIT"
 
   depends_on xcode: ["14.0", :build]
   depends_on macos: :monterey
 
   def install
-    system "swift", "build", "--configuration", "release"
+    system "swift", "build", "--configuration", "release", "--disable-sandbox"
     bin.install ".build/release/eventpanel"
   end
 
--- README.md
+++ README.md
@@ -8,10 +8,12 @@ This is a [Homebrew](https://brew.sh) tap for the EventPanel CLI tool.
 # Add the tap
 brew tap eventpanel/eventpanel
 
+export HOMEBREW_GITHUB_API_TOKEN=xxx
+
 # Install EventPanel
 brew install eventpanel
 ```
 
 ## Documentation
 
-For more information about EventPanel CLI, visit the [main repository](https://github.com/event-panel/eventpanel-cli).
+For more information about EventPanel CLI, visit the [main repository](https://github.com/eventpanel/eventpanel-cli).

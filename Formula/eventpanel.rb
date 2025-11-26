3943b60cfcb3158917aa898e00eb65a025705349
Dmitry Preobrazhenskiy <d.preobrazhenskiy@gmail.com>
2025-11-26 13:30:25 +0200

    Bump to version 0.0.5

--- Formula/eventpanel.rb
+++ Formula/eventpanel.rb
@@ -1,8 +1,8 @@
 class Eventpanel < Formula
   desc "EventPanel CLI tool"
   homepage "https://github.com/eventpanel/eventpanel-cli"
-  url "https://github.com/eventpanel/eventpanel-cli/releases/download/v0.0.3/eventpanel-macos.tar.gz"
-  sha256 "0b42fca6b8944f3264432f643d81404a84be7afd92d7537bceb76ce766566121"
+  url "https://github.com/eventpanel/eventpanel-cli/releases/download/v0.0.5/eventpanel-macos.tar.gz"
+  sha256 "607b63b1c4e0d2219cbac6eb4d619aeee16777c7abe0f131726ef5d8383cc3d4"
   license "MIT"
 
   depends_on :macos

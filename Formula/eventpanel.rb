07d23d47e4c78ebdd90ea7aa1a35adf30659b10f
onsissond <onsissond@gmail.com>
2025-12-17 21:59:49 +0100

    V0.0.8

--- Formula/eventpanel.rb
+++ Formula/eventpanel.rb
@@ -1,8 +1,8 @@
 class Eventpanel < Formula
   desc "EventPanel CLI tool"
   homepage "https://github.com/eventpanel/eventpanel-cli"
-  url "https://github.com/eventpanel/eventpanel-cli/releases/download/v0.0.7/eventpanel-macos.tar.gz"
-  sha256 "dce7a9e329bf472771313ed1b526fb058714748caae23b57dd823c6552e2541b"
+  url "https://github.com/eventpanel/eventpanel-cli/releases/download/v0.0.8/eventpanel-macos.tar.gz"
+  sha256 "07eea0a7cd3b99450884a5117868d5c7ffe5beac4159c1dc1d831f9aabdf37ab"
   license "Fair-Core-1.0-ALv2"
 
   depends_on :macos

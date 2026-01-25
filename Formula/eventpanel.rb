20cfce72d5650903fd76629bb024abffe0b7a99e
Evgenii Sukhanov <onsissond@gmail.com>
2026-01-25 20:40:40 +0100

    eventpanel 0.2.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/eventpanel.rb
+++ Formula/eventpanel.rb
@@ -1,8 +1,8 @@
 class Eventpanel < Formula
   desc "EventPanel CLI tool"
   homepage "https://eventpanel.net"
-  url "https://github.com/eventpanel/eventpanel-cli/releases/download/v0.2.0/eventpanel-macos.tar.gz"
-  sha256 "9e4fed140f18d10f5cea34126266028afe6999da1e880dbbb78bc1892522f081"
+  url "https://github.com/eventpanel/eventpanel-cli/releases/download/v0.2.1/eventpanel-macos.tar.gz"
+  sha256 "6b98d4611fb953794a5f2a3244f8bd246dcaf1104189927542a41b294803b9f4"
   license "Fair-Core-1.0-ALv2"
 
   depends_on :macos
